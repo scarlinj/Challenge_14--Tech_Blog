57cdaafd730a2ba1b59ee5650e6c5ba2fafe259f
Stephen Carlin <scarlinj@gmail.com>
2022-06-22 20:37:32 -0700

    created heroku

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -27,7 +27,7 @@ async function loginFormHandler(event) {
 async function signupFormHandler(event) {
     event.preventDefault();
 
-    // not using username - const username = document.querySelector('#email-signup').value.trim();
+    // use e-mail instead of username - const username = document.querySelector('#email-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
